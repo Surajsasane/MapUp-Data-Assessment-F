13f7a48e8e6e259f86484202cee1d9a82a15dc2f
surajsasane <119850087+Surajsasane@users.noreply.github.com>
2023-12-10 13:47:33 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,6 +13,12 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+    # Pivot the DataFrame
+    result_df = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
+
+    # Set diagonal values to 
+    result_df.values[[range(len(result_df))]*2] = 0
+
 
     return df
 
@@ -28,6 +34,17 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+     #Create a new categorical column 'car_type'
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')],
+                            labels=['low', 'medium', 'high'], right=False)
+
+    # Count occurrences of each car_type
+    type_counts = df['car_type'].value_counts().to_dict()
+
+    # Sort the dictionary alphabetically based on keys
+    sorted_type_counts = dict(sorted(type_counts.items()))
+     return sorted_type_counts
+    
 
     return dict()
 
@@ -43,6 +60,16 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+      # Calculate the mean of 'bus' column
+    bus_mean = df['bus'].mean()
+
+    # Find indices where 'bus' values are greater than twice the mean
+    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
+
+    # Sort the list in ascending order
+    bus_indexes.sort()
+
+    return bus_indexes
 
     return list()
 
@@ -58,6 +85,13 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+ # Calculate the average 'truck' values for each route
+    route_avg_truck = df.groupby('route')['truck'].mean()
+
+    # Filter routes where the average 'truck' value is greater than 7
+    selected_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
+
+    return selected_routes
 
     return list()
 
@@ -73,6 +107,13 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+    # Apply custom conditions to multiply matrix values
+    modified_matrix = matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+
+    # Round values to 1 decimal place
+    modified_matrix = modified_matrix.round(1)
+
+    return modified_matrix
 
     return matrix
 
@@ -88,5 +129,18 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    # Combine 'startDay' and 'startTime' to create a new datetime column 'start_datetime'
+    df['start_datetime'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+
+    # Combine 'endDay' and 'endTime' to create a new datetime column 'end_datetime'
+    df['end_datetime'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+
+    # Calculate the time difference for each record
+    df['time_difference'] = df['end_datetime'] - df['start_datetime']
+
+    # Group by ('id', 'id_2') and check if any record has a time difference less than 24 hours or not spanning 7 days
+    result_series = df.groupby(['id', 'id_2']).apply(lambda group: any(
+        (group['time_difference'] < pd.Timedelta(days=1)) | (group['time_difference'] > pd.Timedelta(days=7))
+    )).rename('incorrect_timestamps')
 
     return pd.Series()
