3b49b68d0d56606013c1f342bf19c8e50c7a2f64
surajsasane <119850087+Surajsasane@users.noreply.github.com>
2023-12-10 13:58:06 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-
-def generate_car_matrix(df)->pd.DataFrame:
+def generate_car_matrix(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Creates a DataFrame  for id combinations.
+    Creates a DataFrame for id combinations.
 
     Args:
         df (pandas.DataFrame)
@@ -12,18 +11,15 @@ def generate_car_matrix(df)->pd.DataFrame:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
-    # Write your logic here
     # Pivot the DataFrame
     result_df = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
 
-    # Set diagonal values to 
+    # Set diagonal values to 0
     result_df.values[[range(len(result_df))]*2] = 0
 
+    return result_df
 
-    return df
-
-
-def get_type_count(df)->dict:
+def get_type_count(df: pd.DataFrame) -> dict:
     """
     Categorizes 'car' values into types and returns a dictionary of counts.
 
@@ -33,8 +29,7 @@ def get_type_count(df)->dict:
     Returns:
         dict: A dictionary with car types as keys and their counts as values.
     """
-    # Write your logic here
-     #Create a new categorical column 'car_type'
+    # Create a new categorical column 'car_type'
     df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')],
                             labels=['low', 'medium', 'high'], right=False)
 
@@ -43,13 +38,10 @@ def get_type_count(df)->dict:
 
     # Sort the dictionary alphabetically based on keys
     sorted_type_counts = dict(sorted(type_counts.items()))
-     return sorted_type_counts
-    
 
-    return dict()
+    return sorted_type_counts
 
-
-def get_bus_indexes(df)->list:
+def get_bus_indexes(df: pd.DataFrame) -> list:
     """
     Returns the indexes where the 'bus' values are greater than twice the mean.
 
@@ -59,8 +51,7 @@ def get_bus_indexes(df)->list:
     Returns:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
-    # Write your logic here
-      # Calculate the mean of 'bus' column
+    # Calculate the mean of 'bus' column
     bus_mean = df['bus'].mean()
 
     # Find indices where 'bus' values are greater than twice the mean
@@ -71,10 +62,7 @@ def get_bus_indexes(df)->list:
 
     return bus_indexes
 
-    return list()
-
-
-def filter_routes(df)->list:
+def filter_routes(df: pd.DataFrame) -> list:
     """
     Filters and returns routes with average 'truck' values greater than 7.
 
@@ -84,8 +72,7 @@ def filter_routes(df)->list:
     Returns:
         list: List of route names with average 'truck' values greater than 7.
     """
-    # Write your logic here
- # Calculate the average 'truck' values for each route
+    # Calculate the average 'truck' values for each route
     route_avg_truck = df.groupby('route')['truck'].mean()
 
     # Filter routes where the average 'truck' value is greater than 7
@@ -93,10 +80,7 @@ def filter_routes(df)->list:
 
     return selected_routes
 
-    return list()
-
-
-def multiply_matrix(matrix)->pd.DataFrame:
+def multiply_matrix(matrix: pd.DataFrame) -> pd.DataFrame:
     """
     Multiplies matrix values with custom conditions.
 
@@ -106,7 +90,6 @@ def multiply_matrix(matrix)->pd.DataFrame:
     Returns:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
-    # Write your logic here
     # Apply custom conditions to multiply matrix values
     modified_matrix = matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
 
@@ -115,10 +98,7 @@ def multiply_matrix(matrix)->pd.DataFrame:
 
     return modified_matrix
 
-    return matrix
-
-
-def time_check(df)->pd.Series:
+def time_check(df: pd.DataFrame) -> pd.Series:
     """
     Use shared dataset-2 to verify the completeness of the data by checking whether the timestamps for each unique (`id`, `id_2`) pair cover a full 24-hour and 7 days period
 
@@ -128,12 +108,11 @@ def time_check(df)->pd.Series:
     Returns:
         pd.Series: return a boolean series
     """
-    # Write your logic here
     # Combine 'startDay' and 'startTime' to create a new datetime column 'start_datetime'
     df['start_datetime'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
 
     # Combine 'endDay' and 'endTime' to create a new datetime column 'end_datetime'
-    df['end_datetime'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+    df['end_datetime'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime']) 
 
     # Calculate the time difference for each record
     df['time_difference'] = df['end_datetime'] - df['start_datetime']
@@ -143,4 +122,4 @@ def time_check(df)->pd.Series:
         (group['time_difference'] < pd.Timedelta(days=1)) | (group['time_difference'] > pd.Timedelta(days=7))
     )).rename('incorrect_timestamps')
 
-    return pd.Series()
+    return result_series
